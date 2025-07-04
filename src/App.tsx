f03af509585cbeef8aff3dfdb8f5d741441474ad
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-04 10:13:33 +0600

    first commit after initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+
+        <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+    </div>
+  );
+};
+
+export default App;
